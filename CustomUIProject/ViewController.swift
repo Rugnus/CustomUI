de250912a29e959064511ad8c254e024d6aa789a
Rugnus <sungur.gasanov@mail.ru>
2022-02-07 22:41:17 +0300

    Initial Commit

---
+++ CustomUIProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomUIProject
+//
+//  Created by Sungur on 07.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
